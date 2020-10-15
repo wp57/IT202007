a1fcf8b222328b4a48ee918d1ac568d1f7a4df5e
Winona Patrick <wp57@afsconnect1.njit.edu>
2020-10-15 12:53:34 -0400

    uppercase labels

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -5,14 +5,14 @@ require_once(__DIR__ . "/../lib/helpers.php");
 ?>
 <nav>
 <ul class="nav">
-    <li><a href="home.php">Home</a></li>
+    <li><a href="home.php">HOME</a></li>
     <?php if (!is_logged_in()): ?>
-        <li><a href="login.php">Login</a></li>
-        <li><a href="register.php">Register</a></li>
+        <li><a href="login.php">LOGIN</a></li>
+        <li><a href="register.php">REGISTER</a></li>
     <?php endif; ?>
     <?php if (is_logged_in()): ?>
-        <li><a href="profile.php">Profile</a></li>
-        <li><a href="logout.php">Logout</a></li>
+        <li><a href="profile.php">PROFILE</a></li>
+        <li><a href="logout.php">LOGOUT</a></li>
     <?php endif; ?>
 </ul>
 </nav>

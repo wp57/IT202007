c613666c4b0965cba0270f5d2e285ae85f0365af
Winona Patrick <wp57@afsconnect1.njit.edu>
2020-10-15 12:54:26 -0400

    uppercase

--- project/login.php
+++ project/login.php
@@ -1,8 +1,8 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <form method="POST">
-    <label for="email">Email:</label>
+    <label for="email">EMAIL:</label>
     <input type="email" id="email" name="email" required/>
-    <label for="p1">Password:</label>
+    <label for="p1">PASSWORD:</label>
     <input type="password" id="p1" name="password" required/>
     <input type="submit" name="login" value="Login"/>
 </form>

58be9584a06af99d3d81cfda5a83af0b035d9bbc
winonapatrick <53202207+winonapatrick@users.noreply.github.com>
2020-10-14 22:30:19 -0400

    editing nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,16 +1,18 @@
-
+<link rel="stylesheet" href="static/css/styles.css">
 <?php
-session_start();//we can start our session here so we don't need to worry about it on other pages
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
-<ul>
+<nav>
+<ul class="nav">
     <li><a href="home.php">Home</a></li>
-    <?php if(!is_logged_in()):?>
-    <li><a href="sample_login.php">Login</a></li>
-    <li><a href="sample_reg.php">Register</a></li>
-    <?php endif;?>
-    <?php if(is_logged_in()):?>
-    <li><a href="logout.php">Logout</a></li>
+    <?php if (!is_logged_in()): ?>
+        <li><a href="login.php">Login</a></li>
+        <li><a href="register.php">Register</a></li>
+    <?php endif; ?>
+    <?php if (is_logged_in()): ?>
+        <li><a href="profile.php">Profile</a></li>
+        <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
+</nav>

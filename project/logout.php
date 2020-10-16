f5cb4940b6c497afad8fccea5143f1ec7f008056
Winona Patrick <wp57@afsconnect1.njit.edu>
2020-10-15 22:57:08 -0400

    username and pw validation

--- project/logout.php
+++ project/logout.php
@@ -1,12 +1,13 @@
-
 <?php
-//starts/loads a session, basically tells php to do its magic
 session_start();
 // remove all session variables
 session_unset();
 // destroy the session
 session_destroy();
-echo "You're logged out (proof by dumping the session)<br>";
-echo "<pre>" . var_export($_SESSION, true) . "</pre>";
 ?>
-<a href="home.php">Home</a>
+<?php require_once(__DIR__ . "/partials/nav.php");/*ultimately, this is just here for the function to be loaded now*/ ?>
+<?php
+
+flash("You have been logged out");
+die(header("Location: login.php"));
+?>

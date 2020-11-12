221fbbefe0f24e10b3ffd3eec66ad80b0e0b139c
wp57 <wp57@njit.edu>
2020-11-12 15:45:24 -0500

    world acc fix

--- project/test_create_transactions.php
+++ project/test_create_transactions.php
@@ -140,10 +140,10 @@ if (isset($_POST["save"])) {
     switch($actType)
     {
         case "Deposit":
-            do_bank_action($result, $source, ($amount * -1), $actType, $memo);
+            do_bank_action($world, $source, ($amount * -1), $actType, $memo);
             break;
         case "Withdraw":
-            do_bank_action($result, $source, ($amount * -1), $actType, $memo);
+            do_bank_action($world, $source, ($amount * -1), $actType, $memo);
             break;
         case "Transfer":
             do_bank_action($source, $dest, ($amount * -1), $actType, $memo);

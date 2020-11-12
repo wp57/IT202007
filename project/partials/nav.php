323b6782a6948fd468363004080e0ad5bdf64d42
winonapatrick <53202207+winonapatrick@users.noreply.github.com>
2020-11-11 20:15:30 -0500

    editing nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -10,6 +10,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">LOGIN</a></li>
         <li><a href="register.php">REGISTER</a></li>
     <?php endif; ?>
+    <?php if (has_role("Admin")): ?>
+        <li><a href="test_create_accounts.php">Create Account</a></li>
+        <li><a href="test_list_accounts.php">View Account</a></li>
+        <li><a href="test_create_transactions.php">Create Transaction</a></li>
+        <li><a href="test_list_transactions.php">View Transactions</a></li>
+    <?php endif; ?> 
     <?php if (is_logged_in()): ?>
         <li><a href="profile.php">PROFILE</a></li>
         <li><a href="logout.php">LOGOUT</a></li>

310c9a8d656f2a3ae4883fbab04437b9965f0a2d
wp57 <wp57@njit.edu>
2020-11-12 15:59:17 -0500

    world acc fix

--- project/test_create_transactions.php
+++ project/test_create_transactions.php
@@ -12,7 +12,7 @@ $db = getDB();
 $sql = "SELECT id account_number from Accounts";
 $stmt = $db->prepare($sql);
 $stmt->execute();
-$users=$stmt->fetch();
+$users=$stmt->fetchAll();
 ?>
     <h3>Create Transaction</h3>
     <form method="POST">

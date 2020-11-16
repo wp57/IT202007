1e181f3ba6ff05f2351005686d153ba91af2b9ce
wp57 <wp57@njit.edu>
2020-11-16 00:47:47 -0500

    c

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -11,10 +11,10 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
     <?php if(has_role("Admin")): ?>
-        <li><a href="create_account.php">Create Account</a></li>
-        <li><a href="list_accounts.php">View Account</a></li>
-        <li><a href="create_transactions.php">Create a Transaction</a></li>
-        <li><a href="list_transactions.php">View Transactions</a></li>
+        <li><a href="test_create_account.php">Create Account</a></li>
+        <li><a href="test_list_accounts.php">View Account</a></li>
+        <li><a href="test_create_transactions.php">Create a Transaction</a></li>
+        <li><a href="test_list_transactions.php">View Transactions</a></li>
     <?php endif; ?>
 
     <?php if (is_logged_in()): ?>

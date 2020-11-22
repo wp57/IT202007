f8f27ff6e9a8a44ebe25e53ffb789d4dc253a996
wp57 <wp57@njit.edu>
2020-11-22 18:38:13 -0500

    list

--- project/withdraw.php
+++ project/withdraw.php
@@ -33,40 +33,40 @@ $users=$stmt->fetchAll(PDO::FETCH_ASSOC);
 function do_bank_action($account1, $account2, $amountChange, $memo){
   $db = getDB();
   $query = null;
-  $stmt2 = $db->prepare("SELECT id, account_number, user_id, account_type, opened_date, last_updated, balance from Accounts WHERE id like :q");
-  $r2 = $stmt2->execute([":q" => "%$query%"]);
+  $stmt2 = $db->prepare("SELECT id, account_number, user_id, account_type, opened_date, last_updated, balance from Accounts WHERE id = :q");
+  $r2 = $stmt2->execute([":q" => "$query"]);
   if ($r2) {
         $results = $stmt2->fetchAll(PDO::FETCH_ASSOC);
     }
 
-  $a1total = null;
-  $a2total = null;
+  $a1tot = null;
+  $a2tot = null;
   foreach($results as $r)
   {
     if($account1 == $r["id"])
-        $a1total = $r["balance"];
+        $a1tot = $r["balance"];
     if($account2 == $r["id"])
-      $a2total = $r["balance"];
+      $a2tot = $r["balance"];
   }
-  if($a1total+$amountChange >= 0)
+  if($a1tot+$amountChange >= 0)
   {
   	$query = "INSERT INTO `Transactions` (`act_src_id`, `act_dest_id`, `amount`, `action_type`, `expected_total`, `memo`) 
-  	VALUES(:p1a1, :p1a2, :p1change, :type, :a1total, :memo), 
-  			(:p2a1, :p2a2, :p2change, :type, :a2total, :memo)";
+  	VALUES(:p1a1, :p1a2, :p1change, :type, :a1tot, :memo), 
+  			(:p2a1, :p2a2, :p2change, :type, :a2tot, :memo)";
   	
   	$stmt = $db->prepare($query);
   	$stmt->bindValue(":p1a1", $account1);
   	$stmt->bindValue(":p1a2", $account2);
   	$stmt->bindValue(":p1change", $amountChange);
   	$stmt->bindValue(":type", "Deposit");
-  	$stmt->bindValue(":a1total", $a1total+$amountChange);
+  	$stmt->bindValue(":a1tot", $a1tot+$amountChange);
     $stmt->bindValue(":memo", $memo);
   	//flip data for other half of transaction
   	$stmt->bindValue(":p2a1", $account2);
   	$stmt->bindValue(":p2a2", $account1);
   	$stmt->bindValue(":p2change", ($amountChange*-1));
   	$stmt->bindValue(":type", "Deposit");
-  	$stmt->bindValue(":a2total", $a2total-$amountChange);
+  	$stmt->bindValue(":a2tot", $a2tot-$amountChange);
     $stmt->bindValue(":memo", $memo);
   	$result = $stmt->execute();
     if ($result) {
@@ -78,11 +78,11 @@ function do_bank_action($account1, $account2, $amountChange, $memo){
       }
       $stmt = $db->prepare("UPDATE Accounts set balance = :balance where id=:id");
       $r = $stmt->execute([
-         ":balance"=>($a1total+$amountChange),
+         ":balance"=>($a1tot+$amountChange),
          ":id"=>$account1
     	]);
       $r = $stmt->execute([
-         ":balance"=>($a2total-$amountChange),
+         ":balance"=>($a2tot-$amountChange),
          ":id"=>$account2
     	]);
   	return $result;
@@ -102,5 +102,4 @@ if (isset($_POST["save"])) {
     do_bank_action($dest, "000000000000", ($amount * -1), $memo);
 }
 ?>
-</div>
 <?php require(__DIR__ . "/partials/flash.php");

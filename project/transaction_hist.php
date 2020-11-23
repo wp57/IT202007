94bf524a5017a2cac5ac46504df288fee2ac82a7
wp57 <wp57@njit.edu>
2020-11-22 19:20:29 -0500

    hist

--- project/transaction_hist.php
+++ project/transaction_hist.php
@@ -2,49 +2,47 @@
 
 <?php
 $query = "";
-$results = [];
-if (isset($_POST["query"])) {
-    $query = $_POST["query"];
-}
+$res = [];
+if (isset($id)) {
     $db = getDB();
     $UserId = get_user_id();
-    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id = $UserId like  :q LIMIT 10");
-    $r = $stmt->execute([":q" => "%$query%"]);
-    if ($r) {
-        $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id = :q LIMIT 10");
+    $r = $stmt->execute([":q" => "$id"]);
+    if($r){
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
     else {
         flash("There was a problem fetching the results " . var_export($stmt->errorInfo(), true));
     }
-    $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE id like :q");
-  $r2 = $stmt2->execute([":q" => "%$query%"]);
+    $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE id = :q");
+  $r2 = $stmt2->execute([":q" => "$query"]);
   if ($r2) {
-        $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+        $res = $stmt2->fetchAll(PDO::FETCH_ASSOC);
     }
 ?>
 <h3>List Transactions</h3>
 <div class="results">
-    <?php if (count($results) > 0): ?>
+    <?php if (count($res) > 0): ?>
         <div class="list-group">
-            <?php foreach ($results as $r): ?>
+            <?php foreach ($res as $r): ?>
                 <div class="list-group-item">
-                    <?php foreach ($results2 as $r2): ?>
-                    <?php if ($r2["id"] == $r["act_src_id"]): ?>
+                    <?php foreach ($res2 as $r2): ?>
+                    <?php if ($r2["id"] == $r[act_src_id]): ?>
                     <div>
                         <div>Transaction Number:</div>
-                        <div><?php safer_echo($r["id"]); ?></div>
+                        <div><?php safer_echo($r[id]); ?></div>
                     </div>
                     <div>
                         <div>Balance:</div>
-                        <div><?php safer_echo($r["expected_total"]); ?></div>
+                        <div><?php safer_echo($r[expected_total]); ?></div>
                     </div>
                     <div>
                         <div>Account Type:</div>
-                        <div><?php safer_echo($r2["account_type"]); ?></div>
+                        <div><?php safer_echo($r2[account_type]); ?></div>
                     </div>
                     <div>
                         <div>Account Number:</div>
-                            <div><?php safer_echo($r2["account_number"]); ?></div>
+                            <div><?php safer_echo($r2[account_number]); ?></div>
                     </div>
                     <?php endif; ?>
                     <?php endforeach; ?>

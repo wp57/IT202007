976d3bdf445d04eeeac7b75fe881c760ef1eb4ea
wp57 <wp57@njit.edu>
2020-11-22 19:24:20 -0500

    updating links

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -12,7 +12,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if(has_role("Admin")): ?>
         <li><a href="create_account.php">Create Account</a></li>
-        <li><a href="list_accounts.php">View Account</a></li>
+        <li><a href="test_list_accounts.php">View Account</a></li>
         <li><a href="create_transactions.php">Create a Transaction</a></li>
         <li><a href="list_transactions.php">View Transactions</a></li>
     <?php endif; ?>
@@ -20,8 +20,8 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php if (is_logged_in()): ?>
 	<li><a href="profile.php">Profile</a></li>
 	<li><a href="create_checking_account.php">Create Checking Account</a></li>
-        <li><a href="#">List Accounts</a></li>
-        <li><a href="#">Deposit</a></li>
+        <li><a href="list_accounts.php">List Accounts</a></li>
+        <li><a href="deposit.php">Deposit</a></li>
         <li><a href="withdraw.php">Withdraw</a></li>
         <li><a href="#">Transfer</a></li>
         <li><a href="logout.php">Logout</a></li>

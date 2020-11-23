e1313ae04c089ed09bf16b7c09a59764f482fd25
wp57 <wp57@njit.edu>
2020-11-22 19:31:06 -0500

    copying to master

---
+++ project/test_list_accounts.php
@@ -0,0 +1,64 @@
+<?php require_once(__DIR__ . "/partials/nav.php"); ?>
+<?php
+if (!has_role("Admin")) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You don't have permission to access this page");
+    die(header("Location: login.php"));
+}
+?>
+
+<?php
+$query = "";
+$res = [];
+if (isset($_POST["query"])) {
+    $query = $_POST["query"];
+}
+if (isset($_POST["search"]) && !empty($query)) {
+    $db = getDB();
+    $stmt = $db->prepare("SELECT id, account_number, user_id, account_type, opened_date, last_updated, balance from Accounts WHERE id = :q LIMIT 10");
+    $r = $stmt->execute([":q" => "$query"]);
+    if ($r) {
+        $res = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    else {
+        flash("There was a problem fetching the results");
+    }
+}
+?>
+<h3>List Accounts</h3>
+<form method="POST">
+    <input name="query" placeholder="Search" value="<?php safer_echo($query); ?>"/>
+    <input type="submit" value="Search" name="search"/>
+</form>
+<div class="results">
+    <?php if (count($res) > 0): ?>
+        <div class="list-group">
+            <?php foreach ($res as $r): ?>
+                <div class="list-group-item">
+                    <div>
+                        <div>Account Number</div>
+                        <div><?php safer_echo($r[account_number]); ?></div>
+                    </div>
+                    <div>
+                        <div>Account Type</div>
+                        <div><?php safer_echo($r[account_type]); ?></div>
+                    </div>
+                    <div>
+                        <div>Balance</div>
+                        <div><?php safer_echo($r["balance"]); ?></div>
+                    </div>
+                    <div>
+                        <div>Owner Id</div>
+                        <div><?php safer_echo($r["user_id"]); ?></div>
+                    </div>
+                    <div>
+                        <a type="button" href="edit_accounts.php?id=<?php safer_echo($r['id']); ?>">Edit</a>
+                        <a type="button" href="view_accounts.php?id=<?php safer_echo($r['id']); ?>">View</a>
+                    </div>
+                </div>
+            <?php endforeach; ?>
+        </div>
+    <?php else: ?>
+        <p>No results</p>
+    <?php endif; ?>
+</div>

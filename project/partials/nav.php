2f7c6e1465716f7c2514e67d27bea71e5af62c67
wp57 <wp57@njit.edu>
2020-11-26 02:14:23 -0500

    added tabs and links

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,5 +1,6 @@
 <link rel="stylesheet" href="static/css/styles.css">
 <?php
+// branch
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>

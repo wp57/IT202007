cb9a8c388fdf0ef34dbaa831ccbd4181214079a6
wp57 <wp57@njit.edu>
2020-11-28 23:39:40 -0500

    f

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -1,5 +1,27 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="styles" href="static/css/styles.css">
+
+  </head>
+  <body>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
 <link rel="stylesheet" href="static/css/styles.css">
 <?php
+// branch
 //we'll be including this on most/all pages so it's a good place to include anything else we want on those pages
 require_once(__DIR__ . "/../lib/helpers.php");
 ?>
@@ -12,11 +34,11 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
     <?php if(has_role("Admin")): ?>
         <li><a href="create_account.php">Create Account</a></li>
-	<li><a href="create_transactions.php">Create a Transaction</a></li>
+        <li><a href="create_transactions.php">Create a Transaction</a></li>
     <?php endif; ?>
 
     <?php if (is_logged_in()): ?>
-	<li><a href="create_checking_account.php">Create Checking Account</a></li>
+        <li><a href="create_checking_account.php">Create Checking Account</a></li>
         <li><a href="list_accounts.php">List Accounts</a></li>
         <li><a href="deposit.php">Deposit</a></li>
         <li><a href="withdraw.php">Withdraw</a></li>
@@ -26,3 +48,5 @@ require_once(__DIR__ . "/../lib/helpers.php");
     <?php endif; ?>
 </ul>
 </nav>
+
+

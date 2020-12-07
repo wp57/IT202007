58baab95bbd3245baa102f815bad65371efaf555
wp57 <wp57@njit.edu>
2020-12-06 23:20:07 -0500

    fixing nav to show full nav

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -41,7 +41,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
-	 <?php if(has_role("Admin")): ?>
+	 <?php if(is_logged_in()): ?>
         <li><a href="create_account.php">Create Account</a></li>
         <li><a href="create_transactions.php">Create a Transaction</a></li>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>

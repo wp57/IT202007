1ae0d4b36f3797a9b49d1d396d5a895c3de4323e
wp57 <wp57@njit.edu>
2020-12-07 18:57:00 -0500

    updating to the right version. it was missing dest field

--- project/transfer.php
+++ project/transfer.php
@@ -10,7 +10,7 @@ if ($r) {
         $u = $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 ?>
-    <form method="POST" style = "height: 400px">
+    <form method="POST" style = "height: 450px">
 <div class = "heading4">
 <h3>Make a Transfer</h3>
 </div>
@@ -21,31 +21,35 @@ if ($r) {
             <?php endforeach; ?>
         </select>
         <br>
+        <select name="dest">
+            <?php foreach($u as $user): ?>
+	      <option value="" disabled selected>Destination Account</option>
+              <option value="<?= $user['id']; ?>"><?= $user['account_number']; ?></option>
+            <?php endforeach; ?>
+        </select>
+        <br>
         <input type="float" placeholder="Amount" min="0.00" name="amount"/>
         <br>
         <input type="text" placeholder="Attach optional message" name="memo"/>
         <br>
         <input type="submit" name="save" value="Create"/>
     </form>
-
 <?php
 if (isset($_POST["save"])) {
     $amount = (float)$_POST["amount"];
     $source = $_POST["source"];
     $memo = $_POST["memo"];
     $user = get_user_id();
-    $db = getDB();
-    $sql = "SELECT DISTINCT id from Accounts where account_number = '000000000000'";
-    $stmt = $db->prepare($sql);
-    $stmt->execute();
-    $result=$stmt->fetch();
-    $world = $result["id"];
-if ($amount > 0) {
-    do_bank_action($world, $source, ($amount * -1), $memo, "Transfer");
-}
-else {
-        flash("Error: Value must be positive! Try again.");
-}
+    $dest = $_POST["dest"];
+    if($amount > 0 && $source != $dest)
+      do_bank_action($source, $dest, ($amount * -1), $memo, "Transfer");
+    else
+    {
+      if($amount <= 0)
+	flash("Error: Value must be positive! Try again.");	
+      if($source == $dest)
+        flash("Error: You cannot transfer money to the same account! Try again.");
+    }
 }
 ?>
 </div>

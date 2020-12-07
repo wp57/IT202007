b5d7367510e7c3f25303e9839b6f80349cd349ad
wp57 <wp57@njit.edu>
2020-12-06 23:44:25 -0500

    changing to admin

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -41,9 +41,11 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+	<?php if (has_role("Admin")): ?>
+	 <li><a href="create_account.php">Create Account</a></li>
+           <li><a href="create_transactions.php">Create a Transaction</a></li>
+    <?php endif; ?>
 	 <?php if(is_logged_in()): ?>
-        <li><a href="create_account.php">Create Account</a></li>
-        <li><a href="create_transactions.php">Create a Transaction</a></li>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>
         <li><a href="list_accounts.php">List Accounts</a></li>
         <li><a href="deposit.php">Deposit</a></li>

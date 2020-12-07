dfe4d35f161857312119427431d679810bae5626
wp57 <wp57@njit.edu>
2020-12-06 23:16:58 -0500

    fix nav is logged in func

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -41,7 +41,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
-	 <?php if(has_role("Admin")): ?>
+	 <?php if(is_logged_in()): ?>
         <li><a href="create_account.php">Create Account</a></li>
         <li><a href="create_transactions.php">Create a Transaction</a></li>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>
@@ -49,6 +49,7 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="deposit.php">Deposit</a></li>
         <li><a href="withdraw.php">Withdraw</a></li>
         <li><a href="transfer.php">Transfer</a></li>
+        <li><a href="Transfer2.php">Transfer to Other Users</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>

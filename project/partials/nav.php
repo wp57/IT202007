921182504813a14ef90c10383acf3ab17ea4a22b
wp57 <wp57@njit.edu>
2020-12-06 23:59:09 -0500

    updating permissions

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -41,18 +41,20 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="login.php">Login</a></li>
         <li><a href="register.php">Register</a></li>
     <?php endif; ?>
+	<?php if (has_role("Admin")): ?>
+	 <li><a href="create_account.php">Create Account</a></li>
+           <li><a href="create_transactions.php">Create a Transaction</a></li>
+    <?php endif; ?>
 	 <?php if(is_logged_in()): ?>
-        <li><a href="create_account.php">Create Account</a></li>
-        <li><a href="create_transactions.php">Create a Transaction</a></li>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>
         <li><a href="list_accounts.php">List Accounts</a></li>
         <li><a href="deposit.php">Deposit</a></li>
         <li><a href="withdraw.php">Withdraw</a></li>
         <li><a href="transfer.php">Transfer</a></li>
-        <li><a href="Transfer2.php">Transfer to Other Users</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>
 </ul>
 </div>
     </nav>
+

debeafb15476c520aa3a7005702bd1a39c9cd88e
wp57 <wp57@njit.edu>
2020-12-15 21:52:34 -0500

    replace old logic w do bank action

--- project/create_savings_account.php
+++ project/create_savings_account.php
@@ -11,44 +11,40 @@
         <input type="submit" name="save" value="Create"/>
     </form>
     <?php
-    if(isset($_POST["save"])){
+    if(isset($_POST["save"])) {
         //TODO add proper validation/checks
         $db = getDB();
         $aNum = rand(000000000001, 999999999999);
-        for($x = strlen($aNum); $x < 12; $x++)
-        {
+        for ($x = strlen($aNum); $x < 12; $x++) {
             $aNum = ("0" . $aNum);
         }
         $aType = "Savings";
         $user = get_user_id();
         $apy = 0.03;
         $balance = $_POST["balance"];
-        if($balance >= 5)
-        {
+        if ($balance >= 5) {
             do {
                 $stmt = $db->prepare("INSERT INTO Accounts (account_number, account_type, user_id, balance, apy) VALUES(:aNum, :aType, :user, :balance, :apy)");
                 $r = $stmt->execute([
-                    ":aNum"=>$aNum,
-                    ":aType"=>$aType,
-                    ":user"=>$user,
+                    ":aNum" => $aNum,
+                    ":aType" => $aType,
+                    ":user" => $user,
                     ":apy" => $apy,
-                    ":balance"=>$balance
+                    ":balance" => $balance
                 ]);
                 $aNum = rand(000000000000, 999999999999);
-                for($x = strlen($aNum); $x < 12; $x++)
-                {
+                for ($x = strlen($aNum); $x < 12; $x++) {
                     $aNum = ("0" . $aNum);
                 }
                 $e = $stmt->errorInfo();
-            }while($e[0] == "23000");
+            } while ($e[0] == "23000");
             $numOfMonths = 1;
             $lastId = $db->lastInsertId();
             $stmt = $db->prepare("UPDATE Accounts set nextAPY = TIMESTAMPADD(MONTH,:months,opened_date) WHERE id = :id");
-            $r = $stmt->execute([":id"=>$lastId, ":months"=>$numOfMonths]);
-            if($r){
+            $r = $stmt->execute([":id" => $lastId, ":months" => $numOfMonths]);
+            if ($r) {
                 flash("Your savings account was successfully created with id: " . $lastId . "!");
-            }
-            else{
+            } else {
                 $e = $stmt->errorInfo();
                 flash("Sorry, there was an error creating: " . var_export($e, true));
             }
@@ -60,48 +56,26 @@
                 $result = $stmt2->fetchAll(PDO::FETCH_ASSOC);
             }
             $a1tot = null;
-            foreach($result as $r)
-            {
-                if($r["id"] == 0)
+            foreach ($result as $r) {
+                if ($r["id"] == 0)
                     $a1tot = $r["balance"];
             }
-
-            $query = "INSERT INTO `Transactions` (`act_src_id`, `act_dest_id`, `amount`, `action_type`, `expected_total`)
-        VALUES(:p1a1, :p1a2, :p1change, :type, :a1tot),
-                        (:p2a1, :p2a2, :p2change, :type, :a2tot)";
-
-            $stmt = $db->prepare($query);
-            $stmt->bindValue(":p1a1", 0);
-            $stmt->bindValue(":p1a2", $lastId);
-            $stmt->bindValue(":p1change", ($balance*-1));
-            $stmt->bindValue(":type", "Deposit");
-            $stmt->bindValue(":a1tot", $a1tot-$balance);
-            //flip data for other half of transaction
-            $stmt->bindValue(":p2a1", $lastId);
-            $stmt->bindValue(":p2a2", 0);
-            $stmt->bindValue(":p2change", $balance);
-            $stmt->bindValue(":type", "Deposit");
-            $stmt->bindValue(":a2tot", $balance);
-            $result = $stmt->execute();
-            if ($result) {
-                flash("Transaction created successfully");
+            $sql = "SELECT DISTINCT id from Accounts where account_number = '000000000000'";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $result = $stmt->fetch();
+            $world = $result["id"];
+            $memo = "Savings";
+            do_bank_action($world, $lastId, ($balance*-1), $memo, "Deposit");
             }
-            else {
-                $e = $stmt->errorInfo();
-                flash("Error creating: " . var_export($e, true));
-            }
-            $stmt = $db->prepare("UPDATE Accounts SET balance = (SELECT SUM(amount) FROM Transactions WHERE Transactions.act_src_id = Accounts.id)");
-            $r = $stmt->execute();
-            die(header("Location: list_accounts.php"));
-        }
         else
-        {
-            flash('Balance must be $5.00 or more! Please try again.');
-        }
+            {
+                flash('Balance must be $5.00 or more! Please try again.');
+            }
+        
     }
     ?>
     </div>
 <?php require(__DIR__ . "/partials/flash.php");
-
 
 

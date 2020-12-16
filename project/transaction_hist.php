781cdcc7581aa0703cc55eb7b3546cbfe9f32fbc
wp57 <wp57@njit.edu>
2020-12-16 00:38:42 -0500

    admin can view any acc transac history

--- project/transaction_hist.php
+++ project/transaction_hist.php
@@ -1,23 +1,23 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <div class = "list">
-<?php
-if (isset($_GET["id"])) {
-    $id = $_GET["id"];
-}
-$typeOfTran = array("Withdraw","Deposit","Transfer", "ext-Transfer");
-?>
-<div class = "heading">
-<h3>Transactions History</h3>
-</div>
+    <?php
+    if (isset($_GET["id"])) {
+        $id = $_GET["id"];
+    }
+    $typeOfTran = array("Withdraw","Deposit","Transfer", "ext-Transfer");
+    ?>
+    <div class = "heading">
+        <h3>Transactions History</h3>
+    </div>
 
     <form method="POST" style = "height: 400px; width: 370px; padding: 50px 15px 0px 0px; margin: 0px 0px 0px 150px">
-	<div class = "heading2">
-	<h3>Filter Transactions</h3>
-	</div>
+        <div class = "heading2">
+            <h3>Filter Transactions</h3>
+        </div>
         <select name="transaction">
             <?php foreach($typeOfTran as $transaction): ?>
-              <option value="" disabled selected>Transaction Type</option>
-              <option value="<?= $transaction; ?>"><?= $transaction; ?></option>
+                <option value="" disabled selected>Transaction Type</option>
+                <option value="<?= $transaction; ?>"><?= $transaction; ?></option>
             <?php endforeach; ?>
         </select>
         <br>
@@ -27,7 +27,7 @@ $typeOfTran = array("Withdraw","Deposit","Transfer", "ext-Transfer");
         <br>
         <input type="submit" name="filter" value="Filter"/>
     </form>
-<?php
+    <?php
 $query = "";
 $result = [];
 if (isset($id)) {
@@ -42,7 +42,7 @@ if (isset($id)) {
         catch(Exception $e){
         }
     }
-    
+
     $stmt = $db->prepare("SELECT count(*) as total from Transactions WHERE act_src_id like :q ORDER BY id DESC LIMIT 10");
     $r = $stmt->execute([":q" => "%$id%"]);
     $result = $stmt->fetch(PDO::FETCH_ASSOC);
@@ -52,7 +52,7 @@ if (isset($id)) {
     }
     $total_pages = ceil($total / $per_page);
     $offset = ($page-1) * $per_page;
-    
+
     $stmt = $db->prepare("SELECT * from Transactions WHERE act_src_id like :q ORDER BY id DESC LIMIT :offset, :count");
     //need to use bindValue to tell PDO to create these as ints
     //otherwise it fails when being converted to strings (the default behavior)
@@ -64,18 +64,18 @@ if (isset($id)) {
         if($e[0] != "00000"){
             flash(var_export($e, true), "alert");
         }
-    
+
     if(isset($_POST["filter"]) || isset($_SESSION['isFiltered'])){
       if(isset($_POST["transaction"])){
         $_SESSION["tranAction"] = $_POST["transaction"];
         $actType = $_SESSION["tranAction"];
       }
       else{
-	if(isset($_SESSION["tranAction"])){
-        	$actType = $_SESSION["tranAction"];
-      	}
+        if(isset($_SESSION["tranAction"])){
+                $actType = $_SESSION["tranAction"];
+        }
       }
-	$firstDate = "0000-01-01";
+        $firstDate = "0000-01-01";
         $secDate = "9999-12-31";
 
       if(isset($_POST["firstDate"]) || isset($_POST["secDate"])){//not set def
@@ -93,20 +93,20 @@ if (isset($id)) {
         }
       }//set
       elseif(isset($_POST["filter"])){
-	if(isset($_SESSION["isFiltered"])){ 
+        if(isset($_SESSION["isFiltered"])){
           $firstDate = $_SESSION["first"];
           $secDate = $_SESSION["sec"];
       }
-	}
-      else{        
-	$_SESSION["first"] = "0000-01-01";
+        }
+      else{
+        $_SESSION["first"] = "0000-01-01";
         $_SESSION["sec"] = "9999-12-31";
         $firstDate = $_SESSION["first"];
         $secDate = $_SESSION["sec"];
       }
-      
+
       $_SESSION["isFiltered"] = true;
-      
+
       if($actType != ""){
         $stmt = $db->prepare("SELECT count(*) as total from Transactions WHERE (action_type like :a) AND (act_src_id like :q) AND (created BETWEEN :f AND :s) ORDER BY id DESC LIMIT 10");
         $r = $stmt->execute([":q" => "%$id%", ":a" => $actType, ":f" => $firstDate, ":s" => $secDate]);
@@ -117,7 +117,7 @@ if (isset($id)) {
         }
         $total_pages = ceil($total / $per_page);
         $offset = ($page-1) * $per_page;
-        
+
         $stmt = $db->prepare("SELECT * from Transactions WHERE (action_type like :a) AND (act_src_id like :q) AND (created BETWEEN :f AND :s) ORDER BY id DESC LIMIT :offset, :count");
         $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
         $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
@@ -137,7 +137,7 @@ if (isset($id)) {
         }
         $total_pages = ceil($total / $per_page);
         $offset = ($page-1) * $per_page;
-        
+
         $stmt = $db->prepare("SELECT * from Transactions WHERE (act_src_id like :q) AND (created BETWEEN :f AND :s) ORDER BY id DESC LIMIT :offset, :count");
         $stmt->bindValue(":offset", $offset, PDO::PARAM_INT);
         $stmt->bindValue(":count", $per_page, PDO::PARAM_INT);
@@ -146,82 +146,116 @@ if (isset($id)) {
         $stmt->bindValue(":q", $id);
       }
     }
-    
+
     $r = $stmt->execute();
     if ($r) {
         $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
-    
+
     $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE user_id = :userId");
     $r2 = $stmt2->execute([":userId" => $userId]);
     if ($r2) {
         $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
     }
+
+
+    $yourAcc = false;
+    foreach($results2 as $thisU)
+    {
+      if($thisU['id'] == $id)
+        $yourAcc = true;
+    }
+
+    if(has_role("Admin"))
+    {
+      $yourAcc = true;
+      $stmt2 = $db->prepare("SELECT id, account_number, account_type from Accounts WHERE id = :id");
+      $r2 = $stmt2->execute([":id" => $id]);
+      if ($r2) {
+          $results2 = $stmt2->fetchAll(PDO::FETCH_ASSOC);
+      }
+    }
+
 }
 ?>
-<div class = "heading">
-  <label>Filtered By: <br>
-  <?php 
-    if(isset($_SESSION['isFiltered'])):
-      if($_SESSION['isFiltered']):
-        if($actType != ""):
-          echo "Transaction type: " . $actType;
-        endif;
-      endif;
-    endif;
-    ?> <br> Dates:
-    <?php 
-    if(isset($_SESSION['isFiltered'])):
-      if($_SESSION['isFiltered']):
-        echo $firstDate . " to  " . $secDate;
-      endif;
-    else:
-      echo "All Time";
-    endif; ?>
-    </label>
+    <div class = "heading">
+        <label>Filtered By: <br>
+            <?php
+            if(isset($_SESSION['isFiltered'])):
+            if($_SESSION['isFiltered']):
+            if($actType != ""):
+            echo "Transaction type: " . $actType;
+            endif;
+            endif;
+            endif;
+            ?> <br> Dates:
+            <?php
+            if(isset($_SESSION['isFiltered'])):
+            if($_SESSION['isFiltered']):
+            echo $firstDate . " to  " . $secDate;
+            endif;
+            else:
+            echo "All Time";
+            endif; ?>
+        </label>
+        <?php if ($yourAcc): ?>
     <?php if (count($results) > 0): ?>
         <div class="list-group">
             <?php foreach ($results as $r): ?>
-                <div class="list-group-item">
-                    <?php foreach ($results2 as $r2): ?>
+            <div class="list-group-item">
+                <?php foreach ($results2 as $r2): ?>
                     <?php if ($r2["id"] == $r["act_src_id"]): ?>
-		    <div>
-                        <div>Transaction Number:</div>
-                        <div><?php safer_echo($r["id"]); ?></div>
-                    </div>
-                    <div>
-                        <div>Balance:</div>
-                        <div><?php safer_echo($r["expected_total"]); ?></div>
-                    </div>
-                    <div>
-                        <div>Account Type:</div>
-                        <div><?php safer_echo($r2["account_type"]); ?></div>
-                    </div>
-                    <div>
-                        <div>Account Number:</div>
-                            <div><?php safer_echo($r2["account_number"]); ?></div>
-                    </div>
-		    <?php endif; ?>
-                    <?php endforeach; ?>
+                <div>
+                    <div>Transaction Number:</div>
+                    <div><?php safer_echo($r["id"]); ?></div>
                 </div>
-		<br>
+                <div>
+                    <div>Balance:</div>
+                    <div><?php safer_echo($r["expected_total"]); ?></div>
+                </div>
+                <div>
+                    <div>Account Type:</div>
+                    <div><?php safer_echo($r2["account_type"]); ?></div>
+                </div>
+                <div>
+                    <div>Account Number:</div>
+                    <div><?php safer_echo($r2["account_number"]); ?></div>
+                </div>
+                <?php endif; ?>
+                    <?php endforeach; ?>
+            </div>
+        <br>
             <?php endforeach; ?>
         </div>
+        <?php else: ?>
+            <p>No results</p>
+        <?php endif; ?>
     <?php else: ?>
-        <p>No results</p>
-    <?php endif; ?>
+            <div class="heading">
+                <h3>The account history you are trying to view is not yours.<h3>
+            </div>
+        <?php endif; ?>
+    </div>
+    <nav aria-label="Trans History">
+        <ul class="pagination justify-content-center">
+            <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
+                <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
+            </li>
+            <?php for($i = 0; $i < $total_pages; $i++):?>
+            <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
+            <?php endfor; ?>
+            <li class="page-item <?php echo ($page+1) > $total_pages?"disabled":"";?>">
+                <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page+1;?>">Next</a>
+            </li>
+        </ul>
+    </nav>
 </div>
-  <nav aria-label="Trans History">
-    <ul class="pagination justify-content-center">
-      <li class="page-item <?php echo ($page-1) < 1?"disabled":"";?>">
-        <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page-1;?>" tabindex="-1">Previous</a>
-      </li>
-      <?php for($i = 0; $i < $total_pages; $i++):?>
-      <li class="page-item <?php echo ($page-1) == $i?"active":"";?>"><a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo ($i+1);?>"><?php echo ($i+1);?></a></li>
-      <?php endfor; ?>
-      <li class="page-item <?php echo ($page+1) > $total_pages?"disabled":"";?>">
-        <a class="page-link" href="?id=<?php echo $id;?>&page=<?php echo $page+1;?>">Next</a>
-      </li>
-    </ul>
-  </nav>
-</div>
+
+
+
+
+
+
+
+
+

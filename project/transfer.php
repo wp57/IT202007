19af37456e60ebeb60fbd5981b025c3607fd1ad0
wp57 <wp57@njit.edu>
2020-12-15 22:08:15 -0500

    merge conflict, matching w loan branch

--- project/transfer.php
+++ project/transfer.php
@@ -1,30 +1,36 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
 <div class="big">
-<?php
-$db = getDB();
-$id = get_user_id();
-$u = [];
-$stmt = $db->prepare("SELECT * FROM Accounts WHERE active = 'Active' AND user_id = :id");
-$r = $stmt->execute([":id" => "$id"]);
-if ($r) {
+    <?php
+    $db = getDB();
+    $id = get_user_id();
+    $u = [];
+    $stmt = $db->prepare("SELECT * from Accounts WHERE (account_type != 'Loan') AND user_id = :id");
+    $r = $stmt->execute([":id" => "$id"]);
+    if ($r) {
         $u = $stmt->fetchAll(PDO::FETCH_ASSOC);
-}
-?>
+    }
+    $u2 = [];
+    $stmt = $db->prepare("SELECT * from Accounts WHERE user_id = :id");
+    $r = $stmt->execute([":id" => "$id"]);
+    if ($r) {
+        $u2 = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+    ?>
     <form method="POST" style = "height: 450px">
-<div class = "heading4">
-<h3>Make a Transfer</h3>
-</div>
+        <div class = "heading4">
+            <h3>Make a Transfer</h3>
+        </div>
         <select name="source">
             <?php foreach($u as $user): ?>
-             <option value="" disabled selected>Account</option>
-              <option value="<?= $user["id"]; ?>"><?= $user["account_number"]; ?></option>
+                <option value="" disabled selected>Account</option>
+                <option value="<?= $user["id"]; ?>"><?= $user["account_number"]; ?></option>
             <?php endforeach; ?>
         </select>
         <br>
         <select name="dest">
-            <?php foreach($u as $user): ?>
-	      <option value="" disabled selected>Destination Account</option>
-              <option value="<?= $user['id']; ?>"><?= $user['account_number']; ?></option>
+            <?php foreach($u2 as $user): ?>
+                <option value="" disabled selected>Destination Account</option>
+                <option value="<?= $user['id']; ?>"><?= $user['account_number']; ?></option>
             <?php endforeach; ?>
         </select>
         <br>
@@ -34,6 +40,7 @@ if ($r) {
         <br>
         <input type="submit" name="save" value="Create"/>
     </form>
+</div>
 <?php
 if (isset($_POST["save"])) {
     $amount = (float)$_POST["amount"];
@@ -41,30 +48,31 @@ if (isset($_POST["save"])) {
     $memo = $_POST["memo"];
     $user = get_user_id();
     $dest = $_POST["dest"];
-    
 
- $stmt2 = $db->prepare("SELECT balance FROM Accounts WHERE id = :id");
+
+    $stmt2 = $db->prepare("SELECT * FROM Accounts WHERE id = :id");
     $r2 = $stmt2->execute([
-       ":id"=>$source
-      ]);
-	$result = $stmt2->fetch(PDO::FETCH_ASSOC);
-	$a1tot = $result["balance"];    
-       // echo $a1tot; 
-if($amount > 0 && $source != $dest) {
+        ":id" => $source
+    ]);
+    $result = $stmt2->fetch(PDO::FETCH_ASSOC);
+    $a1tot = $result["balance"];
+    $aType = $result["account_type"];
+    
+    if ($amount > 0) {
         if ($amount <= $a1tot) {
-	do_bank_action($source, $dest, ($amount * -1), $memo, "Transfer");
-        }
-	elseif($source == $dest){
-        flash("Error: You cannot transfer money to the same account! Try again.");
-        }
-        elseif ($amount > $a1tot){
-            flash("Error: You do not have enough money to make this transfer.");
-        }
-	}
-else {
+            do_bank_action($source, $dest, ($amount * -1), $memo, "Transfer");
+        } elseif ($source == $dest) {
+            flash("Error: You cannot transfer money to the same account! Try again.");
+        } elseif ($amount > $a1tot) {
+            flash("Error: You do not have enough money to make this withdrawal.");
+        } elseif ($aType == 'Loan') {
+	    flash("Error: You cannot transfer money from a loan account.");
+    } else {
         flash("Error: Value must be positive!");
-     }
+    }
 }
-?>
-</div>
-<?php require(__DIR__ . "/partials/flash.php");
+}
+    ?>
+    </div>
+    <?php require(__DIR__ . "/partials/flash.php");
+

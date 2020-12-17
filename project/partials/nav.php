c3b41a9444d09d7ddcdc58c5c7c2115e5bea8655
wp57 <wp57@njit.edu>
2020-12-17 00:07:53 -0500

    moved admin only tabs to admin only section

--- project/partials/nav.php
+++ project/partials/nav.php
@@ -44,6 +44,12 @@ require_once(__DIR__ . "/../lib/helpers.php");
 	<?php if (has_role("Admin")): ?>
 	 <li><a href="create_account.php">Create Account</a></li>
            <li><a href="create_transactions.php">Create a Transaction</a></li>
+		<li><a href="search4.php">Search for User</a></li>
+        <li><a href="search3.php">Search for Account</a></li>
+        <li><a href="freeze.php">Freeze an Account</a></li>
+        <li><a href="closeAdmin.php">Close a User's Account</a></li>
+        <li><a href="deactivate.php">Deactivate a User's Account</a></li>
+
     <?php endif; ?>
 	 <?php if(is_logged_in()): ?>
         <li><a href="create_checking_account.php">Create Checking Account</a></li>
@@ -54,11 +60,6 @@ require_once(__DIR__ . "/../lib/helpers.php");
         <li><a href="withdraw.php">Withdraw</a></li>
         <li><a href="transfer.php">Transfer</a></li>
 	<li><a href="Transfer2.php">Transfer to Other Users</a></li>
-	<li><a href="search4.php">Search for User</a></li>
-	<li><a href="search3.php">Search for Account</a></li>
-	<li><a href="freeze.php">Freeze an Account</a></li>
-	<li><a href="closeAdmin.php">Close a User's Account</a></li>
-	<li><a href="deactivate.php">Deactivate a User's Account</a></li>
         <li><a href="profile.php">Profile</a></li>
         <li><a href="logout.php">Logout</a></li>
     <?php endif; ?>

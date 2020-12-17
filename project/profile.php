283ea94dfd2f79cf24eeb33309b6ff49d4665a01
wp57 <wp57@njit.edu>
2020-12-17 00:24:09 -0500

    merge conflict again

--- project/profile.php
+++ project/profile.php
@@ -1,21 +1,22 @@
 <?php require_once(__DIR__ . "/partials/nav.php"); ?>
-<div class="big">
-    <?php
-    if (!is_logged_in()) {
-        //this will redirect to login and kill the rest of this script (prevent it from executing)
-        flash("You must be logged in to access this page");
-        die(header("Location: login.php"));
-    }
-    $id = get_user_id();
-    if (isset($_GET["id"])) {
-        $id = $_GET["id"];
-    }
-    $db = getDB();
-    $stmt = $db->prepare("SELECT email, username, first_name, last_name, visible from Users WHERE id = :id LIMIT 1");
-    $stmt->execute([":id" => $id]);
-    $results = $stmt->fetch(PDO::FETCH_ASSOC);
-    $vis = $results['visible'];
-    if (isset($_POST["saved"])) {
+    <div class="big">
+<?php
+if (!is_logged_in()) {
+    //this will redirect to login and kill the rest of this script (prevent it from executing)
+    flash("You must be logged in to access this page");
+    die(header("Location: login.php"));
+}
+$id = get_user_id();
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+}
+$db = getDB();
+$stmt = $db->prepare("SELECT email, username, first_name, last_name, visible from Users WHERE id = :id LIMIT 1");
+$stmt->execute([":id" => $id]);
+$results = $stmt->fetch(PDO::FETCH_ASSOC);
+$vis = $results['visible'];
+
+if (isset($_POST["saved"])) {
     $isValid = true;
     //check if our email changed
     $newEmail = get_email();
@@ -41,7 +42,7 @@
         else {
             $newEmail = $email;
         }
-    }
+
 
     $newUsername = get_username();
     if (get_username() != $_POST["username"]) {
@@ -76,6 +77,7 @@
     $newFirstName = get_firstName();
     if ((get_firstName() != $_POST["firstName"])) {
         $newFirstName = $_POST["firstName"];
+        }
         $newLastName = get_lastName();
         if ((get_lastName() != $_POST["lastName"])) {
             $newLastName = $_POST["lastName"];
@@ -85,122 +87,115 @@
             $vis = $_POST["visible"];
         }
     }
-        if ($isValid) {
-            $stmt = $db->prepare("UPDATE Users set email = :email, username= :username, first_name= :firstName, last_name= :lastName, visible = :visible where id = :id");
-            $r = $stmt->execute([":email" => $newEmail, ":username" => $newUsername,":firstName" => $newFirstName, ":lastName" => $newLastName, ":visible" => $vis, ":id" => get_user_id()]);
-            if ($r) {
-                flash("Updated profile");
-            }
-            else {
-                flash("Error updating profile");
-            }
-            if (!empty($_POST["password"]) && !empty($_POST["confirm"]) && !empty($_POST["current"])) {
-                $curr = $_POST["current"];
-                $stmt = $db->prepare("SELECT password from Users WHERE id = :userid");
-                $stmt->execute([":userid" => get_user_id()]);
-                $result = $stmt->fetch(PDO::FETCH_ASSOC);
-                if ($result && isset($result["password"])) {
-                    $password_hash_from_db = $result["password"];
-                    if(password_verify($curr, $password_hash_from_db))
+    if ($isValid) {
+        $stmt = $db->prepare("UPDATE Users set email = :email, username= :username, first_name= :firstName, last_name= :lastName, visible = :visible where id = :id");
+        $r = $stmt->execute([":email" => $newEmail, ":username" => $newUsername,":firstName" => $newFirstName, ":lastName" => $newLastName, ":visible" => $vis, ":id" => get_user_id()]);
+        if ($r) {
+            flash("Updated profile");
+        }
+        else {
+            flash("Error updating profile");
+        }
+        if (!empty($_POST["password"]) && !empty($_POST["confirm"]) && !empty($_POST["current"])) {
+            $curr = $_POST["current"];
+            $stmt = $db->prepare("SELECT password from Users WHERE id = :userid");
+            $stmt->execute([":userid" => get_user_id()]);
+            $result = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($result && isset($result["password"])) {
+                $password_hash_from_db = $result["password"];
+                if(password_verify($curr, $password_hash_from_db))
+                {
+                    if ($_POST["password"] == $_POST["confirm"])
                     {
-                        if ($_POST["password"] == $_POST["confirm"])
+                        if(strlen($_POST["password"]) >= 8)
                         {
-                            if(strlen($_POST["password"]) >= 8)
-                            {
-                                $password = $_POST["password"];
-                                $hash = password_hash($password, PASSWORD_BCRYPT);
-                                $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
-                                $r = $stmt->execute([":id" => get_user_id(), ":password" => $hash]);
-                                if ($r) {
-                                    flash("Reset Password");
-                                }
-                                else {
-                                    flash("Error resetting password");
-                                }
+                            $password = $_POST["password"];
+                            $hash = password_hash($password, PASSWORD_BCRYPT);
+                            $stmt = $db->prepare("UPDATE Users set password = :password where id = :id");
+                            $r = $stmt->execute([":id" => get_user_id(), ":password" => $hash]);
+                            if ($r) {
+                                flash("Reset Password");
                             }
-                            else if(strlen($_POST["password"]) < 5)
-                            {
-                                flash("New password must be at least 5 characters.");
+                            else {
+                                flash("Error resetting password");
                             }
                         }
-                        else
+                        else if(strlen($_POST["password"]) < 5)
                         {
-                            flash("New passwords do not match.");
+                            flash("New password must be at least 5 characters.");
                         }
                     }
                     else
                     {
-                        flash("Current password is incorrect.");
+                        flash("New passwords do not match.");
                     }
                 }
-            }
-//fetch/select fresh data in case anything changed
-            $stmt = $db->prepare("SELECT email, username, first_name, last_name, visible from Users WHERE id = :id LIMIT 1");
-            $stmt->execute([":id" => get_user_id()]);
-            $result = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($result) {
-                $email = $result["email"];
-                $username = $result["username"];
-                $firstName = $result["first_name"];
-                $lastName = $result["last_name"];
-                $vis = $result["visible"];
-                //let's update our session too
-                $_SESSION["user"]["email"] = $email;
-                $_SESSION["user"]["username"] = $username;
-                $_SESSION["user"]["first_name"] = $firstName;
-                $_SESSION["user"]["last_name"] = $lastName;
-                $_SESSION["user"]["visible"] = $vis;
+                else
+                {
+                    flash("Current password is incorrect.");
+                }
             }
         }
-        else {
-            //else for $isValid, though don't need to put anything here since the specific failure will output the message
+//fetch/select fresh data in case anything changed
+        $stmt = $db->prepare("SELECT email, username, first_name, last_name, visible from Users WHERE id = :id LIMIT 1");
+        $stmt->execute([":id" => get_user_id()]);
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+        if ($result) {
+            $email = $result["email"];
+            $username = $result["username"];
+            $firstName = $result["first_name"];
+            $lastName = $result["last_name"];
+            $vis = $result["visible"];
+            //let's update our session too
+            $_SESSION["user"]["email"] = $email;
+            $_SESSION["user"]["username"] = $username;
+            $_SESSION["user"]["first_name"] = $firstName;
+            $_SESSION["user"]["last_name"] = $lastName;
+            $_SESSION["user"]["visible"] = $vis;
         }
     }
-    ?>
-    <?php if($vis == 'Public' && $id != get_user_id()): ?>
+    else {
+        //else for $isValid, though don't need to put anything here since the specific failure will output the message
+
+    }
+}
+?>
+<?php if($vis == 'Public' && $id != get_user_id()): ?>
     <div class="heading"
     <h3>Profile</h3>
-</div>
-<?php safer_echo("First Name: " . $results['first_name']); ?>
-<?php safer_echo("Last Name: " . $results['last_name']); ?>
+    </div>
+    <?php safer_echo("First Name: " . $results['first_name']); ?>
+    <?php safer_echo("Last Name: " . $results['last_name']); ?>
 
-<?php safer_echo("Username: " . $results['username']); ?>
+    <?php safer_echo("Username: " . $results['username']); ?>
 <?php endif; ?>
 <?php if($id == get_user_id()): ?>
 
-<form method="POST" style = "height: 700px; width: 400px;">
-<div class="heading"
-<h3>Edit Your Profile</h3>
-</div>
-<input type="text" placeholder="First Name" name="firstName" value="<?php safer_echo(get_firstName()); ?>"/>
-<br>
-<input type="text" name="lastName" placeholder="Last Name" value="<?php safer_echo(get_lastName()); ?>"/>
-<br>
-<input type="email" placeholder="Email" name="email" value="<?php safer_echo(get_email()); ?>"/>
-<br>
-<input type="text" placeholder="Username" maxlength="60" name="username" value="<?php safer_echo(get_username()); ?>"/>
-<!-- DO NOT PRELOAD PASSWORD-->
-<input type="password" placeholder="Current Password" name="current"/>
-<br>
-<input type="password" placeholder="New Password" name="password"/>
-<br>
-<input type="password" placeholder="Confirm Password" name="confirm"/>
-<br>
-<select name="visible">
-    <option value="" disabled selected>Privacy</option>
-    <option value="Public">Public</option>
-    <option value="Private">Private</option>
-</select>
-<input type="submit" name="saved" value="Save Profile"/>
+    <form method="POST" style = "height: 700px; width: 400px;">
+    <div class="heading"
+    <h3>Edit Your Profile</h3>
+    </div>
+    <input type="text" placeholder="First Name" name="firstName" value="<?php safer_echo(get_firstName()); ?>"/>
+    <br>
+    <input type="text" name="lastName" placeholder="Last Name" value="<?php safer_echo(get_lastName()); ?>"/>
+    <br>
+    <input type="email" placeholder="Email" name="email" value="<?php safer_echo(get_email()); ?>"/>
+    <br>
+    <input type="text" placeholder="Username" maxlength="60" name="username" value="<?php safer_echo(get_username()); ?>"/>
+    <!-- DO NOT PRELOAD PASSWORD-->
+    <input type="password" placeholder="Current Password" name="current"/>
+    <br>
+    <input type="password" placeholder="New Password" name="password"/>
+    <br>
+    <input type="password" placeholder="Confirm Password" name="confirm"/>
+    <br>
+    <select name="visible">
+        <option value="" disabled selected>Privacy</option>
+        <option value="Public">Public</option>
+        <option value="Private">Private</option>
+    </select>
+    <input type="submit" name="saved" value="Save Profile"/>
 <?php endif; ?>
-</form>
+    </form>
     </div>
 <?php require(__DIR__ . "/partials/flash.php");
-
-
-
-
-
-
-
-
